28900b6d4440827f8f1966e1df5459e0e1def9c6
Jonathan Sepkuty <jonathansepkuty@gmail.com>
2025-11-21 13:46:34 +0200

    Added lecture 2 code examples and notes

---
+++ lectures/lecture2/hogwarts.py
@@ -0,0 +1,10 @@
+students = [
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
+]
+
+for student in students:
+    print(student["name"], "is in", student["house"])
+
