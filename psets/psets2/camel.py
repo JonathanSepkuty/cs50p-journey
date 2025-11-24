96eef7eb939d123d36caf9780cda61f5fa30fb27
JonathanSepkuty <jonathansepkuty@gmail.com>
2025-11-24 12:00:54 +0200

    added more notes and problem solutions

---
+++ psets/psets2/camel.py
@@ -0,0 +1,10 @@
+user=input("camelCase: ")
+def camel_to_snake(camel_str):
+    snake_str=""
+    for char in camel_str:
+        if char.isupper():
+            snake_str+="_"+char.lower()
+        else:
+            snake_str+=char
+    return snake_str.lstrip("_")
+print ("snake_case "+ camel_to_snake(user))
